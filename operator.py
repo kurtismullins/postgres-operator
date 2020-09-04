947ad02922ad61769cedf41845ae44610a785517
Kurtis Mullins <kurtis.mullins@gmail.com>
2020-09-04 16:00:35 -0400

    WIP - Just a stub

---
+++ operator.py
@@ -0,0 +1,19 @@
+import time
+
+from kubernetes import client, config
+
+
+config.load_kube_config()
+
+
+if __name__ == '__main__':
+
+    while True:
+
+        v1 = client.CoreV1Api()
+        print("Listing Pods")
+        pods = v1.list_pod_for_all_namespaces(watch=False)
+        for p in pods:
+            print("Pod Name: %s\tNamespace: %s" % (pod.metadata.name, pod.metadata.namespace)
+
+        time.sleep(120)
